f72d9cefc23ec4dcd622e3ebb3faa1aa08e2b400
Iakov <108896775+IakovEu@users.noreply.github.com>
2025-01-09 22:25:41 +0300

    Delete практические_15 directory

--- практические_15/1&2.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.btn').addEventListener('click', ()=>{
-    document.querySelector('.btn__img-1').classList.toggle('active');
-    document.querySelector('.btn__img-2').classList.toggle('active');
-})
-
-document.querySelector('.btn-1').addEventListener('click', ()=>{
-    alert(`width - ${window.screen.width}, height - ${window.screen.height}`);
-})
